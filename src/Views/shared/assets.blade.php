f977649c75633b0fc5293d4bdce8e0727f8a7f76
James <bannis67@hotmail.com>
2016-07-21 20:24:33 +1200

    Update assets.blade.php

--- src/Views/shared/assets.blade.php
+++ src/Views/shared/assets.blade.php
@@ -11,7 +11,7 @@
         }
     }
     loadCSS({!! '"'.asset('//cdn.datatables.net/plug-ins/505bef35b56/integration/bootstrap/3/dataTables.bootstrap.css').'"' !!});
-    loadCSS({!! '"'.asset('//cdn.datatables.net/responsive/1.0.7/css/responsive.bootstrap.min.css').'"' !!});
+    {{-- loadCSS({!! '"'.asset('//cdn.datatables.net/responsive/1.0.7/css/responsive.bootstrap.min.css').'"' !!}); --}}
     @if($editor_enabled)
         loadCSS({!! '"'.asset('https://cdnjs.cloudflare.com/ajax/libs/summernote/' . Kordy\Ticketit\Helpers\Cdn::Summernote . '/summernote.css').'"' !!});
         @if($include_font_awesome)
